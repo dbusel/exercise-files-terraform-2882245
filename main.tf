0db1824f10ae64b01983f9991971e37172179ab1
dbusel <10116634+dbusel@users.noreply.github.com>
2024-01-16 19:43:00 +0100

    Update main.tf with security group

--- main.tf
+++ main.tf
@@ -18,12 +18,53 @@ provider "aws" {
   region  = "us-west-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.micro"
+
+  vpc_security_group_ids = ["aws_security_group.blog.id"]
 
   tags = {
     Name = "Hello World"
   }
 }
     
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow HTTP and HTTPS in, allow everything out"
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}

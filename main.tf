5337adebebcb2979e7b76200b101356ae42590ac
dbusel <10116634+dbusel@users.noreply.github.com>
2024-01-16 20:16:14 +0100

    Update main.tf with recent fixes

--- main.tf
+++ main.tf
@@ -40,11 +40,11 @@ module "blog_sg" {
 
   vpc_id = data.aws_vps.default.id
 
-  ingress_rules      = ["http-80-tcp", "http-443-tcp"]
-  ingress_cidr_rules = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "http-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules      = ["all-all"]
-  egress_cidr_rules = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
     
 resource "aws_security_group" "blog" {
